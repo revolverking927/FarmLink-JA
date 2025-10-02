a7552082b07eb0439b59de90aa96e225d284f1ca
revolverking927 <skeyondre12@gmail.com>
2025-10-01 19:06:52 -0500

    Completed login and register, database and node server

---
+++ public/js/user-form.js
@@ -0,0 +1,90 @@
+const buyerBtn = document.querySelector('#buyer-btn');
+const farmerBtn = document.querySelector('#farmer-btn');
+
+console.log(buyerBtn);
+
+
+const firstname = document.querySelector('#first-name') || null;
+const lastname = document.querySelector('#last-name');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const parish = document.querySelector('#parish');
+// const license = document.querySelector('#food-handlers-license');
+const submitBtn = document.querySelector('#submit-btn');
+
+let currRole = 'farmer';
+
+//form validation
+
+if (firstname == null && lastname == null) { //means login page is open
+    submitBtn.addEventListener('click', () => {
+        fetch('/login-user',{
+            method: 'post',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify({
+                email: email.value,
+                password: password.value
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            validateData(data);
+        })
+    })
+} else { //means register page is open
+
+    buyerBtn.addEventListener('click', () => {//when buyer button is clicked
+        if (currRole === 'farmer') {
+            // license.setAttribute('hidden', true);
+        }
+        currRole = 'buyer';
+    });
+
+    farmerBtn.addEventListener('click', () => {//when farmer button is clicked
+        if (currRole === 'buyer') {
+            // license.removeAttribute('hidden');
+        }
+        currRole = 'farmer';
+    });
+
+    submitBtn.addEventListener('click', () => {//form submission button
+        fetch('/register-user', { //used to submit the form
+            method: 'post',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify({
+                firstname: firstname.value,
+                lastname: lastname.value,
+                email: email.value,
+                password: password.value,
+                parish: parish.value,
+                role: currRole,
+            }) 
+        }) 
+        .then(res => res.json()) //response to json
+        .then(data => {
+            validateData(data);
+        })
+    })
+}
+
+const alertBox = (data) => {
+    const alertContainer = document.querySelector('.alert-box');
+    const alertMsg = document.querySelector('.alert-message');
+    alertMsg.innerHTML = data;
+
+    alertContainer.style.top = '5%';
+    setTimeout(() => {
+        alertContainer.style.top = null;
+    }, 5000);
+}
+
+const validateData = (data) => {
+    if(!data.firstname) {
+        alertBox(data);
+    } else {
+        sessionStorage.name = data.name;
+        sessionStorage.email = data.email;
+        location.href = '/'
+    }
+}
+
--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const knex = require('knex');
+const express = require('express'); //Express js is use to create server
+const path = require('path'); //path allow the code to know the files locations
+const bodyParser = require('body-parser'); //body-parser allow the code to send and recieve data
+const knex = require('knex'); //knex will allow the cde to access the database(farmlink)
+// const cors = require('cors');
 
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1', //database host number
+        user: 'postgres', //psql user name
+        password: '12.comQw', //password
+        database: 'farmlink' //database name
+    }
+})
 const app = express();
+// app.use(cors());
 
 let initialPath = path.join(__dirname, "public");
 
@@ -24,6 +36,54 @@ app.get('/register', (req, res) => {
 
 app.get('/marketplace', (req, res) => {
     res.sendFile(path.join(initialPath, "marketplace.html"));
+})
+
+app.post('/register-user', (req, res) => {
+    //gives access to the variables in the request
+    const {firstname, lastname, email, password, parish, role} = req.body; 
+
+    if (!firstname || !lastname || !email || !password || !parish || !role) {
+        res.json('fill all the fields');
+    } else {
+        db("users").insert({
+            firstname: firstname,
+            lastname: lastname,
+            email: email,
+            password: password, 
+            parish: parish,
+            role: role,
+        })
+        .returning(["firstname", "lastname", "email"])
+        .then(data => {
+            res.json(data[0])
+        })
+        .catch(err => {
+            if (err.code === '23505') { // unique_violation in Postgres
+                return res.json('email already exists');
+            } else {
+                console.error(err);
+                return res.status(500).json('Database error');
+            }
+        }) //error handler
+        
+    }
+})
+
+app.post('/login-user', (req, res) => {
+    const { email, password} = req.body;
+     db.select('firstname', 'lastname', 'email')
+     .from('users')
+     .where({
+        email: email,
+        password: password
+     })
+     .then(data => {
+        if(data.length){
+            res.json(data[0]);
+        } else{
+            res.json('email or password is incorect')
+        }
+     })
 })
 
 app.listen(3000, (req, res) => {
